a5cd1117a2589886d0a84734ba03ade3411c3569
Eric Gundrum <git@ericgundrum.com>
2017-03-30 14:33:27 -0700

    Initial card flip animation using CSS with hard-coded card width...
    Based on https://davidwalsh.name/css-flip
    with `ngStyle` modifications gleaned from
    https://juristr.com/blog/2016/01/learning-ng2-dynamic-styles/#adding-a-single-class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'card shell'
+  flipped = false
+  width = 300
+
+  flip(): void {
+    this.flipped = !this.flipped
+  }
 }
