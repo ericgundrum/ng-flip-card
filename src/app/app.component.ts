ae6bf6aa979f6d953e517c6c1ac2e34a866ac12f
Eric Gundrum <git@ericgundrum.com>
2017-03-30 19:08:51 -0700

    Determine card width (and set flip width) at runtime...
    `md-card` chooses a width at runtime. The CSS for `flip-container`
    must know this width to determine the flip axis.
    
    At runtime, `@ViewChild` grabs the div bounding the `md-card`,
    retrieves its width, and passes it along to the `flip-container` CSS.
    
    This implementation is based on clues gleaned from
    - https://angular.io/docs/ts/latest/api/core/index/ViewChild-decorator.html
    - http://stackoverflow.com/a/35209681
    - https://plnkr.co/edit/RJJdcY?p=preview
    - http://stackoverflow.com/a/40072227

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core'
+import { AfterViewInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'card shell'
   flipped = false
-  width = 300
+  width: number
+  @ViewChild('front') front: ElementRef
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.width = this.front.nativeElement.offsetWidth
+    this.cdr.detectChanges()
+  }
 
   flip(): void {
     this.flipped = !this.flipped
